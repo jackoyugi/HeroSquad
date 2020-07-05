6f24c3fe5dbde09ef78ebd5f6f5871ae3a650665
jackoyugi <oyugijack@gmail.com>
2020-07-05 21:22:28 +0300

    added new images and logo at layout.hbs

---
+++ src/main/resources/public/js/script.js
@@ -0,0 +1,27 @@
+var start = math.floor(Math.random()*100);
+var start1 = math.floor(Math.random()*100);
+var start1 = math.floor(Math.random()*100);
+
+function count(countTo, countContainer, parentContainer) {
+    var i =0;
+    var interval = setInterval(function() {
+    if (i ===countTo) clearInterval(interval);
+    $(countContainer).html(i++);
+    $(countContainer).css("colo", "#ff5b" + i + "");
+    $(parentContainer).css("border-color", "#ff5b" + i + "44");
+    $(parentContainer).css("border-top-color", "transparent");
+    $(".back").css("background", "#ff5b" + i "5b");
+    }, 50);
+}
+$(document).ready(function() {
+    count(start, ".def-cnt", ".defence");
+    count(start1, ".atc-cnt", ".attack");
+    count(start2, ".dis-cnt", ".distance");
+
+    $(".addMember").click(function(){
+    $("#newMember").css("left", "0%");
+    });
+    $(".close").click(function(){
+    $("#newMember").css("left", "100%");
+    });
+});
