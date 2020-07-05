7b048c1857bee75303c2838b8967d066f8b5f170
jackoyugi <oyugijack@gmail.com>
2020-07-05 14:15:13 +0300

    updated the UI interface at App.java by adding home page and form geter methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,12 @@
 import models.Hero;
 import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
 public class App {
 
@@ -10,5 +16,16 @@ public class App {
         Hero.setUpNewHero1();
         Squad.setUpNewSquad();
         Squad.setUpNewSquad1();
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero-form", (req, re) ->{
+                Map<String, Object> model = new HashMap<>();
+                return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
     }
+
 }
